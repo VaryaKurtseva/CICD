d96b20b72b3892a84f0ea0709853ee990a62db11
Varyyaass <varyakurtseva@yandex.ru>
2025-06-15 11:06:04 +0300

    Удаляю лишние строк в классе User.java

--- src/main/java/org/example/User.java
+++ src/main/java/org/example/User.java
@@ -7,21 +7,20 @@ public class User {
     private String name;
     private String password;
 
-    // Конструктор для регистрации по email
     public User(String username, String email, String password) {
         this.name = username;
         this.email = email;
         this.password = password;
     }
 
-    // Конструктор для регистрации по телефону
+   
     public User(String username, String phone, String password, boolean byPhone) {
         this.name = username;
         this.phone = phone;
         this.password = password;
     }
 
-    // Конструктор для регистрации через соцсеть
+    
     public User(String username, String socialProvider) {
         this.name = username;
         this.socialProvider = socialProvider;
